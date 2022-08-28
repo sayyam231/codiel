3418958ba016595c3e00cf87c9dbd06ddb722a69
sayyam231 <sayyamjain.it1@bvp.edu.in>
2022-08-28 14:49:03 +0530

    Set up view engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 // use express Router
 app.use('/', require('./routes/index'));
 
+// set view engine
+app.set('view engine', 'ejs');
+app.set('view', './views');
+
 app.listen(port, (err) => {
     if (err) {
         // console.log('Error : ', err); There's a another way to console.log
