f099354f0c79d2ede1ac29e978c98e3d3327ac3c
sayyam231 <sayyamjain.it1@bvp.edu.in>
2022-08-28 14:22:28 +0530

    Add home route and controller

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// use express Router
+app.use('/', require('./routes/index'));
+
 app.listen(port, (err) => {
     if (err) {
         // console.log('Error : ', err); There's a another way to console.log
