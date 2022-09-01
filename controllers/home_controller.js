3ebef2db07178e80da14ccc554a1a6e08c767d55
sayyam231 <sayyamjain.it1@bvp.edu.in>
2022-09-01 23:43:54 +0530

    show all the post including the user name with populate

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,19 @@
+const Post = require('../models/post');
+
 module.exports.home = function (req, res) {
-    return res.render('home', {
-        title: "Home"
+    
+    // populate the user to show the name too
+    Post.find({}).populate('user').exec(function (err, posts) {
+        if (err) {
+            console.log("err in finding post");
+        }
+        return res.render('home', {
+            title: "Home",
+            posts: posts
+        });
     });
+        
+    
+   
+    
 };
