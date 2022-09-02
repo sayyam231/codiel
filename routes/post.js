c610e5415b863d9ddc790a411719f9bcabd3e678
sayyam231 <sayyamjain.it1@bvp.edu.in>
2022-09-02 14:26:14 +0530

    add user check for creating post

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const postController = require('../controllers/post-controller');
-router.post('/create', postController.create);
+router.post('/create',passport.checkAuthentication, postController.create);
 
 module.exports = router;
