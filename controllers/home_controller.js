b1d2e0b7c53ed4a3ce7ba952ab11005abcac97ec
sayyam231 <sayyamjain.it1@bvp.edu.in>
2022-09-02 16:15:11 +0530

    Display comments and user details on web page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,18 @@
+const { populate } = require('../models/comments');
 const Post = require('../models/post');
 
 module.exports.home = function (req, res) {
     
     // populate the user to show the name too
-    Post.find({}).populate('user').exec(function (err, posts) {
+    Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comment',
+            populate: {
+                path: 'user'
+            }
+        })
+        .exec(function (err, posts) {
         if (err) {
             console.log("err in finding post");
         }
