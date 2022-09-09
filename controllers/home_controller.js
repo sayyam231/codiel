e200568fead6fa878276e4689693fd7318dcd31a
sayyam231 <sayyamjain.it1@bvp.edu.in>
2022-09-09 13:15:04 +0530

    Change the home controller to async await and error handling

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,28 +1,27 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-module.exports.home = function (req, res) {
-    
+module.exports.home =async function (req, res) {
+    try {
+        let posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comment',
+                populate: {
+                    path: 'user'
+                }
+            });
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: "Home",
+            posts: posts,
+            all_users: users
+        });
+    } catch (err) {
+        console.log("Error", err);
+        return;
+    }
     // populate the user to show the name too
-    Post.find({})
-        .populate('user')
-        .populate({
-            path: 'comment',
-            populate: {
-                path: 'user'
-            }
-        })
-        .exec(function (err, posts) {
-            User.find({}, (err, user)=> {
-                return res.render('home', {
-                    title: "Home",
-                    posts: posts,
-                    all_users:user
-                });
-         })
-        
-    });
-        
     
-   
     
 };
